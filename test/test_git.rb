30fead193dea6ce465d9c6b54490c40a7a2a8d9e
Yegor Bugayenko <yegor256@gmail.com>
2019-06-12 17:26:03 +0300

    Up to 2019, Happy New Year!

--- test/test_git.rb
+++ test/test_git.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 #
-# Copyright (c) 2014-2018 Teamed.io
-# Copyright (c) 2014-2018 Yegor Bugayenko
+# Copyright (c) 2014-2019 Teamed.io
+# Copyright (c) 2014-2019 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -27,7 +27,7 @@ require 'tmpdir'
 
 # Git test.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2014-2018 Yegor Bugayenko
+# Copyright:: Copyright (c) 2014-2019 Yegor Bugayenko
 # License:: MIT
 class TestGit < Minitest::Test
   def test_parsing
