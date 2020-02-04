b7b1bf75cc7bad709d3f09d0e77d7707f11c2fa9
Yegor Bugayenko <yegor256@gmail.com>
2020-02-04 08:48:01 +0300

    Up to 2020, Happy New Year!

--- .simplecov
+++ .simplecov
@@ -1,7 +1,7 @@
 # encoding: utf-8
 #
-# Copyright (c) 2014-2019 Teamed.io
-# Copyright (c) 2014-2019 Yegor Bugayenko
+# Copyright (c) 2014-2020 Teamed.io
+# Copyright (c) 2014-2020 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
--- lib/hoc.rb
+++ lib/hoc.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2019 Yegor Bugayenko
+# Copyright (c) 2014-2020 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -23,7 +23,7 @@ require 'hoc/svn'
 
 # HOC main module.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2014-2019 Yegor Bugayenko
+# Copyright:: Copyright (c) 2014-2020 Yegor Bugayenko
 # License:: MIT
 module HOC
   # Code base abstraction
